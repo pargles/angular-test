ffc564fac845022574d00418e270a5651f886406
Pargles Dall'Oglio <pargles@rossintelligence.com>
2021-02-15 18:20:42 -0500

    session 12: interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { CustomersModule } from './customers/customers.module';
+import { SharedModule } from  './shared/shared.module';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CustomersModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, CustomersModule, SharedModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
