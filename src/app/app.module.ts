95e98f7b79207b6ef43aa177869eb66f16808799
Pargles Dall'Oglio <pargles@rossintelligence.com>
2021-02-15 16:45:49 -0500

    couple more adjustments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// import { NgModule }      from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { CustomersModule } from './customers/customers.module';
+// import { AppComponent }  from './app.component';
+
+// @NgModule({
+//   imports:      [ BrowserModule, CustomersModule ],
+//   declarations: [ AppComponent ],
+//   bootstrap:    [ AppComponent ]
+// })
+// export class AppModule { }
