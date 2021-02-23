78175bcd95ec87d8e8d580cee72c284640519344
Pargles Dall'Oglio <pargles@rossintelligence.com>
2021-02-22 19:43:07 -0500

    lesson 33

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { SharedModule } from './shared/shared.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
-  imports:      [ HttpClientModule,BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule ],
+  imports:      [ HttpClientModule,BrowserModule, OrdersModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
